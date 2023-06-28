740e63411e772dbeef9aedf81974054839591cee
Carey Janecka <figitaki96@yahoo.com>
2023-06-28 14:05:32 -0700

    nvim-tree config

--- lua/figivim/plugins/nvimtree.lua
+++ lua/figivim/plugins/nvimtree.lua
@@ -6,9 +6,16 @@ local M = {
 }
 
 M.config = function()
-  require 'nvim-tree'.setup()
+  require 'nvim-tree'.setup {
+    filters = { custom = { "^.git$" } }
+  }
 
   vim.keymap.set({ 'n', 'v', 'i' }, '<C-t>', ':NvimTreeToggle<CR>')
+
+  local api = require('nvim-tree.api')
+  api.events.subscribe(api.events.Event.FileCreated, function(file)
+    vim.cmd("edit " .. file.fname)
+  end)
 end
 
 return M
