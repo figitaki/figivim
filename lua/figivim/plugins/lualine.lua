9d38d95b897810f05eef301122360fabdd8f4b1b
Carey Janecka <figitaki96@yahoo.com>
2023-06-28 13:30:19 -0700

    add lualine, fix other errors

---
+++ lua/figivim/plugins/lualine.lua
@@ -0,0 +1,10 @@
+local M = {
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+}
+
+M.opts = {}
+
+return M
--- lua/figivim/plugins/noice.lua
+++ lua/figivim/plugins/noice.lua
@@ -1,16 +1,16 @@
 local M = {
-  "folke/noice.nvim",
-  event = "VeryLazy",
+  'folke/noice.nvim',
+  event = 'VeryLazy',
   opts = {
     cmdline = {
-      view = "cmdline"
+      view = 'cmdline'
     },
     routes = {
       {
         filter = {
-          event = "msg_show",
-          kind = "",
-          find = "written",
+          event = 'msg_show',
+          kind = '',
+          find = 'written',
         },
         opts = { skip = true },
       },
@@ -19,9 +19,9 @@ local M = {
       -- override markdown rendering so that **cmp** and other plugins
       -- use **Treesitter**
       override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
     },
     -- you can enable a preset for easier configuration
@@ -34,12 +34,12 @@ local M = {
     },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
+    -- if you lazy-load any plugin below, make sure to add proper `module='...'` entries
+    'MunifTanjim/nui.nvim',
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    'rcarriga/nvim-notify',
   }
 }
 
