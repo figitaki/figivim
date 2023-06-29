078ced5ef000bf9f90ebc0fda0a4659bf9ff37e5
Carey Janecka <figitaki96@yahoo.com>
2023-06-28 19:38:49 -0700

    deprecate mini files

--- lua/figivim/plugins/mini.lua
+++ lua/figivim/plugins/mini.lua
@@ -1,7 +1,10 @@
 local M = { 'echasnovski/mini.nvim', version = false }
 
 M.config = function()
-  require('mini.starter').setup()
+  require('mini.files').setup()
+  vim.keymap.set({ 'n', 'v' }, '<C-t>', function()
+    MiniFiles.open()
+  end)
 end
 
 return M
