a8767adc223b9601e37142b275bca511c92aa84e
Carey Janecka <careyjanecka@gmail.com>
2025-01-03 12:36:45 -0800

    disable haskell, add mini keybind

--- lua/figivim/plugins/mini.lua
+++ lua/figivim/plugins/mini.lua
@@ -6,6 +6,13 @@ M.config = function()
   vim.keymap.set({ 'n', 'v' }, '<C-t>', function()
     MiniFiles.open()
   end)
+  vim.keymap.set({ 'n', 'v' }, '<leader>ft', function()
+    local _ = MiniFiles.close()
+        or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+    vim.defer_fn(function()
+      MiniFiles.reveal_cwd()
+    end, 30)
+  end)
 end
 
 return M
